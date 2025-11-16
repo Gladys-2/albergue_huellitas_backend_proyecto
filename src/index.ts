7bb7983d5b4d97370f88fc3409d2803db97647e9
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-16 17:41:15 -0400

    feat: crear servidor Express con rutas y manejo de errores

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { sequelize } from "./config/db";
+import { AppDataSource } from "./config/db";
 
 import authRoutes from "./auth/routes/auth.routes";
 import userRoutes from "./usuarios/routes/user.routes";
@@ -11,7 +11,6 @@ import adopcionRoutes from "./adopciones/routes/adopcion.routes";
 import refugioRoutes from "./refugios/routes/refugio.routes";
 import voluntarioRoutes from "./voluntarios/routes/voluntario.routes";
 import administradorRoutes from "./administradores/routes/administrador.routes";
-
 
 import "./usuarios/entities/usuario.entity";
 import "./administradores/entities/administrador.entity";
@@ -26,16 +25,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://192.168.0.7:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/usuarios", userRoutes);
@@ -46,23 +43,25 @@ app.use("/api/refugios", refugioRoutes);
 app.use("/api/voluntarios", voluntarioRoutes);
 app.use("/api/administradores", administradorRoutes);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("API Refugio Huellitas funcionando correctamente");
+app.get("/", (_req: Request, res: Response) => {
+  res.send("api--> Refugio Huellitas funcionando correctamente");
 });
 
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    message: err.message || "Error interno del servidor",
-  });
-});
+app.use(
+  (err: Error & { status?: number }, _req: Request, res: Response, _next: NextFunction) => {
+    console.error("Error global:", err.stack);
+    res.status(err.status ?? 500).json({
+      message: err.message || "Error interno del servidor",
+    });
+  }
+);
 
 const testConnection = async () => {
   try {
-    await sequelize.authenticate();
-    console.log("Conexión a PostgreSQL exitosa");
+    await AppDataSource.initialize();
+    console.log("Conexión a PostgreSQL exitosa con TypeORM");
   } catch (error) {
-    console.error("Error al conectar a PostgreSQL:", error);
+    console.error("Error al conectar a PostgreSQL con TypeORM:", error);
   }
 };
 

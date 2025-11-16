4bdc05c76700e7b9b59cc745457efde7f58a8044
Gladys-2 <gladysninaquispe7@gmail.com>
2025-11-16 17:40:05 -0400

    feat: CRUD de animales y rutas

--- src/animales/entities/animal.entity.ts
+++ src/animales/entities/animal.entity.ts
@@ -1,22 +1,24 @@
-import { DataTypes } from "sequelize";
-import { sequelize } from "../../config/db";
-import Refugio from "../../refugios/entities/refugio.entity";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+import { Refugio } from "../../refugios/entities/refugio.entity";
 
-const Animal = sequelize.define("Animal", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  nombre: { type: DataTypes.STRING, allowNull: false },
-  especie: DataTypes.STRING,
-  estado: DataTypes.STRING,
-  foto: DataTypes.STRING,
-  refugio_id: {
-    type: DataTypes.INTEGER,
-    references: { model: Refugio, key: "id" },
-  },
-}, {
-  tableName: "animales",
-  timestamps: false,
-});
+@Entity("animales")
+export class Animal {
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-Animal.belongsTo(Refugio, { foreignKey: "refugio_id" });
+  @Column({ nullable: true, default: "Sin nombre" })
+  nombre?: string;
 
-export default Animal;
+  @Column({ nullable: true })
+  especie?: string;
+
+  @Column({ nullable: true })
+  estado?: string;
+
+  @Column({ nullable: true })
+  foto?: string;
+
+  @ManyToOne(() => Refugio, { nullable: true })
+  @JoinColumn({ name: "refugio_id" })
+  refugio?: Refugio;
+}
